a1e6a37ad6fbecc63b9ac15c44b806bfa0d6d5ab
Joram Wayne <joramwmuya@gmail.com>
2025-05-25 20:42:23 +0300

    Implement Article model with constructor and save method

--- Lib/models/author.py
+++ Lib/models/author.py
@@ -1,3 +1,56 @@
-class Author:
-    def __init__(self):
-        pass
+from Lib.db.connection import get_connection
+
+class Author :
+
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
+
+    def save(self):
+        conn =get_connection()
+        cursor = conn.cursor ()
+        cursor.execute ("INSERT INTO authors (name) VALUES (?)", (self.name,))
+        self.id =cursor.lastrowid 
+        conn.commit ()
+        conn.close() 
+
+    @classmethod
+    def find_by_id(cls, author_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE id = ?", (author_id,))
+        row = cursor.fetchone()
+        conn.close ()
+        return cls(**row) if row else None
+
+    def articles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self.id,))
+        return cursor.fetchall()
+
+    def magazines(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.* FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        return cursor.fetchall()
+
+    def add_article(self, magazine, title):
+        from Lib.models.article import Article
+        article = Article(title=title, author_id=self.id, magazine_id=magazine.id)
+        article.save()
+        return article
+
+    def topic_areas(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.category FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        return [row['category'] for row in cursor.fetchall()]
